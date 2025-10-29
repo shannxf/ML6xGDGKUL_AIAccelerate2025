837bc210185136636eddc7d776ec71628442cab1
Rosaline <rosaline@snoeys.eu>
2025-10-29 18:31:08 +0100

    new test data

--- my_agent/tools/__init__.py
+++ my_agent/tools/__init__.py
@@ -3,6 +3,3 @@ from .read_files import read_png_as_string
 from .image_handling import image_ops
 from .calculator import calculator
 from .date_time import get_time
-from .string_reasoner import string_reasoner
-from .grid_extractor import grid_extractor
-from .code_reasoner import code_reasoner
